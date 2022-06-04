00045ee85d65c19b37098c42fb99f835806f7138
Captsanthosh <55809033+Captsanthosh@users.noreply.github.com>
2022-06-04 15:55:24 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,17 +1,17 @@
-##import streamlit
+import streamlit
 
-##streamlit.title('hello world')
-##streamlit.header('friends')
-##streamlit.text('joey like food')
-##streamlit.text('rachel like style')
-##streamlit.text('monica like perfection')
-##streamlit.text('chandler like sarcastic comments')
-##streamlit.text('phoebe like rare things')
-##streamlit.text('ross like marriage')
+streamlit.title('hello world')
+streamlit.header('friends')
+streamlit.text('joey like food')
+streamlit.text('rachel like style')
+streamlit.text('monica like perfection')
+streamlit.text('chandler like sarcastic comments')
+streamlit.text('phoebe like rare things')
+streamlit.text('ross like marriage')
 
 
-##streamlit.header('FAVOURITE')
-##streamlit.text('HOW U DOING......')
+streamlit.header('FAVOURITE')
+streamlit.text('HOW U DOING......')
 
 
 
