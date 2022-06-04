92c1a3ccbd25c2aede9adebac293d37ef3cdc738
Captsanthosh <55809033+Captsanthosh@users.noreply.github.com>
2022-06-04 15:54:40 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,14 +1,21 @@
-import streamlit
+##import streamlit
 
-streamlit.title('hello world')
-streamlit.header('friends')
-streamlit.text('joey like food')
-streamlit.text('rachel like style')
-streamlit.text('monica like perfection')
-streamlit.text('chandler like sarcastic comments')
-streamlit.text('phoebe like rare things')
-streamlit.text('ross like marriage')
+##streamlit.title('hello world')
+##streamlit.header('friends')
+##streamlit.text('joey like food')
+##streamlit.text('rachel like style')
+##streamlit.text('monica like perfection')
+##streamlit.text('chandler like sarcastic comments')
+##streamlit.text('phoebe like rare things')
+##streamlit.text('ross like marriage')
 
 
-streamlit.header('FAVOURITE')
-streamlit.text('HOW U DOING......')
+##streamlit.header('FAVOURITE')
+##streamlit.text('HOW U DOING......')
+
+
+
+import pandas
+
+my_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_list)
