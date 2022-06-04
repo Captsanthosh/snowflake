7b2ffe8660b4f2af7f592746b2b0e930bf9dc60f
Captsanthosh <55809033+Captsanthosh@users.noreply.github.com>
2022-06-04 15:59:03 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,21 +1,25 @@
 import streamlit
 
-streamlit.title('hello world')
-streamlit.header('friends')
-streamlit.text('joey like food')
-streamlit.text('rachel like style')
-streamlit.text('monica like perfection')
-streamlit.text('chandler like sarcastic comments')
-streamlit.text('phoebe like rare things')
-streamlit.text('ross like marriage')
+#streamlit.title('hello world')
+#streamlit.header('friends')
+#streamlit.text('joey like food')
+#streamlit.text('rachel like style')
+#streamlit.text('monica like perfection')
+#streamlit.text('chandler like sarcastic comments')
+#streamlit.text('phoebe like rare things')
+#streamlit.text('ross like marriage')
 
 
-streamlit.header('FAVOURITE')
-streamlit.text('HOW U DOING......')
+#streamlit.header('FAVOURITE')
+#streamlit.text('HOW U DOING......')
 
 
 
 import pandas
 
 my_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_list)
+
+
+steamlit.multiselect("pick some fruits from list:", list(my_list.index))
 streamlit.dataframe(my_list)
