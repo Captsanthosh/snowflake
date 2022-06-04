992f1731c9ed9eddbc653e7ec006a230881ab204
Captsanthosh <55809033+Captsanthosh@users.noreply.github.com>
2022-06-04 15:34:55 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
 
-streamlit.title('hi karthi')
-streamlit.title('how u doing')
+streamlit.title('hello world')
+streamlit.header('friends')
+streamlit.text('joey like food')
+streamlit.text('rachel like style')
+streamlit.text('monica like perfection')
+streamlit.text('chandler like sarcastic comments')
+streamlit.text('phoebe like rare things')
+streamlit.text('ross like marriage')
